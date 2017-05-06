c9d1d6e5c29fdf06d62cb21c3dad16a17306156d
DeeRids <David_Ridley@baylor.edu>
2017-05-05 21:46:15 -0500

    Second

---
+++ Flexbot-1.5.0_Baylor/FlexBot/src/main/java/com/hexairbot/hexmini/HudViewControllerDelegate.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.hexairbot.hexmini;
+
+import com.hexairbot.hexmini.ui.Button;
+
+import android.view.View;
+
+
+/**
+ * @author koupoo
+ *
+ */
+public interface HudViewControllerDelegate {
+	public void settingsBtnDidClick(View settingsBtn);
+}
---
+++ Flexbot-1.5.0_Baylor/FlexBot/src/main/java/com/vmc/ipc/ftp/FtpConnectedListener.java
@@ -0,0 +1,6 @@
+package com.vmc.ipc.ftp;
+
+public interface FtpConnectedListener {
+    public void onFtpConnect();
+    public void onFtpDisconnect();
+}
